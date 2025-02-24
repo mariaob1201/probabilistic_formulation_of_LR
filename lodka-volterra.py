40b754edf0630a5269bea5c8680854410daebb60
Maria Oros <mariaoros@Marias-MacBook-Pro.local>
2025-02-23 21:15:34 -0600

    ADD lodka volterra case

--- lodka-volterra.py
+++ lodka-volterra.py
@@ -1,5 +1,3 @@
-# Lodka volterra with Bayesian Inferennce of the model params
-# https://en.wikipedia.org/wiki/Lotka–Volterra_equations
 import arviz as az
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,7 +5,6 @@ import pandas as pd
 import pymc as pm
 import pytensor
 import pytensor.tensor as pt
-
 from numba import njit
 from pymc.ode import DifferentialEquation
 from pytensor.compile.ops import as_op
@@ -16,20 +13,18 @@ from scipy.optimize import least_squares
 
 print(f"Running on PyMC v{pm.__version__}")
 
-
-#------------- The Hudson Bay Company data
-# fmt: off
+# ------------- The Hudson Bay Company data
 data = pd.DataFrame(dict(
-    year = np.arange(1900., 1921., 1),
-    lynx = np.array([4.0, 6.1, 9.8, 35.2, 59.4, 41.7, 19.0, 13.0, 8.3, 9.1, 7.4,
-                8.0, 12.3, 19.5, 45.7, 51.1, 29.7, 15.8, 9.7, 10.1, 8.6]),
-    hare = np.array([30.0, 47.2, 70.2, 77.4, 36.3, 20.6, 18.1, 21.4, 22.0, 25.4,
-                 27.1, 40.3, 57.0, 76.6, 52.3, 19.5, 11.2, 7.6, 14.6, 16.2, 24.7])))
+    year=np.arange(1900., 1921., 1),
+    lynx=np.array([4.0, 6.1, 9.8, 35.2, 59.4, 41.7, 19.0, 13.0, 8.3, 9.1, 7.4,
+                   8.0, 12.3, 19.5, 45.7, 51.1, 29.7, 15.8, 9.7, 10.1, 8.6]),
+    hare=np.array([30.0, 47.2, 70.2, 77.4, 36.3, 20.6, 18.1, 21.4, 22.0, 25.4,
+                   27.1, 40.3, 57.0, 76.6, 52.3, 19.5, 11.2, 7.6, 14.6, 16.2, 24.7])))
 print(data.head())
-# fmt: on
+
 
 # plot data function for reuse later
-def plot_data(ax, data, save_as, lw=2, title="Hudson's Bay Company Data"):
+def plot_data(ax, data, save_as=None, lw=2, title="Hudson's Bay Company Data"):
     ax.plot(data.year, data.lynx, color="b", lw=lw, marker="o", markersize=12, label="Lynx (Data)")
     ax.plot(data.year, data.hare, color="g", lw=lw, marker="+", markersize=14, label="Hare (Data)")
     ax.legend(fontsize=14, loc="center left", bbox_to_anchor=(1, 0.5))
@@ -40,6 +35,7 @@ def plot_data(ax, data, save_as, lw=2, title="Hudson's Bay Company Data"):
     ax.set_xticks(data.year.astype(int))
     ax.set_xticklabels(ax.get_xticks(), rotation=45)
     ax.set_title(title, fontsize=16)
+
     if save_as:
         plt.savefig(save_as, bbox_inches='tight')  # Save the figure
         plt.close()  # Close the figure to free up memory
@@ -52,14 +48,8 @@ fig, ax = plt.subplots(figsize=(10, 5))
 plot_data(ax, data, save_as='plot-lodka-volterra/hudsons_bay_plot.png')  # Specify the filename to save
 plt.show()
 
-#------------------- Estimate params of the dynamical system from such data, using odeint
 
-# define the right hand side of the ODE equations in the Scipy odeint signature
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.integrate import odeint
-from numba import njit
-
+# ------------------- Estimate params of the dynamical system from such data, using odeint
 
 @njit
 def rhs(X, t, theta):
@@ -106,7 +96,7 @@ x_y = odeint(func=rhs, y0=initial_conditions, t=time, args=(theta,))
 
 # Plot
 _, ax = plt.subplots(figsize=(12, 4))
-plot_data(ax, data, save_as='plot-lodka-volterra/scipy-model.png', lw=0)
+plot_data(ax, data, save_as='plot-lodka-volterra/scipy-data.png', lw=0)
 plot_model(ax, x_y, save_as='plot-lodka-volterra/model-plot.png')  # Specify filename to save the model plot
 
 plt.show()  # Show the plot if needed
